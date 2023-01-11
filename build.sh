efee11f1f8d561cb4407f8beecb1bf3f283e2cb4
Sai Deekshith <kdeekshithsai7373@gmail.com>
2023-01-11 16:57:36 +0530

    Updated

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo $1
 python3.9 -m pip install --upgrade pip
-apt-get install -y gcc-c++
+apt-get install -y "gcc-c++"
 python3.9 -m pip install -U "cython >= 3.0a6" cffi 'greenlet >= 1.0' setuptools
